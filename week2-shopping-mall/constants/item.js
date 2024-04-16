4a41e020a624a2414b0c73413ea5eaa069f9b45b
suwonthugger <ddww1029wwdd@gmail.com>
2024-04-16 11:07:48 +0900

    feat: 상수 추가

---
+++ week2-shopping-mall/constants/item.js
@@ -0,0 +1,62 @@
+const SHOPPING_LIST = [
+  {
+    id: 1,
+    name: "사과",
+    image: "./assets/apple.jpeg",
+    category: "과일",
+  },
+  {
+    id: 2,
+    name: "레몬",
+    image: "./assets/lemon.jpeg",
+    category: "과일",
+  },
+  {
+    id: 3,
+    name: "오렌지",
+    image: "./assets/orange.jpeg",
+    category: "과일",
+  },
+  {
+    id: 4,
+    name: "파인애플",
+    image: "./assets/pineapple.jpeg",
+    category: "과일",
+  },
+  {
+    id: 5,
+    name: "망고",
+    image: "./assets/mango.jpeg",
+    category: "과일",
+  },
+  {
+    id: 6,
+    name: "카이센동",
+    image: "./assets/kaisendon.jpeg",
+    category: "일식",
+  },
+  {
+    id: 7,
+    name: "돈까스",
+    image: "./assets/porkcutlet.jpeg",
+    category: "일식",
+  },
+  {
+    id: 8,
+    name: "스시",
+    image: "./assets/sushi.jpeg",
+    category: "일식",
+  },
+  {
+    id: 9,
+    name: "돈까스 책",
+    image: "./assets/porkcutlet_book.jpeg",
+    category: "서적",
+  },
+  {
+    id: 10,
+    name: "스시 책",
+    image: "./assets/sushi_book.jpeg",
+    category: "서적",
+  },
+];
