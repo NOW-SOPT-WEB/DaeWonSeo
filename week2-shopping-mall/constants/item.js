bff807836642477827d2a8d7500753338933fb71
suwonthugger <ddww1029wwdd@gmail.com>
2024-04-16 17:03:19 +0900

    feat: nav에 따라서 필터링 하는 기능 구현

--- week2-shopping-mall/constants/item.js
+++ week2-shopping-mall/constants/item.js
@@ -1,62 +1,72 @@
-const SHOPPING_LIST = [
+export const SHOPPING_LIST = [
   {
     id: 1,
     name: "사과",
-    image: "./assets/apple.jpeg",
+    image: "../../assets/apple.jpeg",
     category: "과일",
+    price: 1000, // 가격 추가
   },
   {
     id: 2,
     name: "레몬",
-    image: "./assets/lemon.jpeg",
+    image: "../../assets/lemon.jpeg",
     category: "과일",
+    price: 3000, // 가격 추가
   },
   {
     id: 3,
     name: "오렌지",
-    image: "./assets/orange.jpeg",
+    image: "../../assets/orange.jpeg",
     category: "과일",
+    price: 5000, // 가격 추가
   },
   {
     id: 4,
     name: "파인애플",
-    image: "./assets/pineapple.jpeg",
+    image: "../../assets/pineapple.jpeg",
     category: "과일",
+    price: 7000, // 가격 추가
   },
   {
     id: 5,
     name: "망고",
-    image: "./assets/mango.jpeg",
+    image: "../../assets/mango.jpeg",
     category: "과일",
+    price: 10000, // 가격 추가
   },
   {
     id: 6,
     name: "카이센동",
-    image: "./assets/kaisendon.jpeg",
+    image: "../../assets/kaisendon.jpeg",
     category: "일식",
+    price: 15000, // 가격 추가
   },
   {
     id: 7,
     name: "돈까스",
-    image: "./assets/porkcutlet.jpeg",
+    image: "../../assets/porkcutlet.jpeg",
     category: "일식",
+    price: 12000, // 가격 추가
   },
   {
     id: 8,
     name: "스시",
-    image: "./assets/sushi.jpeg",
+    image: "../../assets/sushi.jpeg",
     category: "일식",
+    price: 50000, // 가격 추가
   },
   {
     id: 9,
     name: "돈까스 책",
-    image: "./assets/porkcutlet_book.jpeg",
+    image: "../../assets/porkcutlet_book.jpeg",
     category: "서적",
+    price: 13000, // 가격 추가
   },
   {
     id: 10,
     name: "스시 책",
-    image: "./assets/sushi_book.jpeg",
+    image: "../../assets/sushi_book.jpeg",
     category: "서적",
+    price: 14000, // 가격 추가
   },
 ];
--- week2-shopping-mall/pages/home/home.js
+++ week2-shopping-mall/pages/home/home.js
@@ -1,5 +1,50 @@
-const mainIcon = document.querySelector("#main-logo");
+import { SHOPPING_LIST } from "../../constants/item.js";
 
+const mainIcon = document.querySelector("#main-logo");
+const itemWrapper = document.querySelector(".items-wrapper");
+const itemTitle = document.querySelector(".item-container h2");
+
+// 메인 로고 클릭 시 home.html로 이동
 mainIcon.onclick = () => {
   window.location.href = "home.html";
 };
+
+// 아이템 리스트 (쇼핑리스트) 를 보여주는 함수
+const displayItems = (filteredItems) => {
+  itemWrapper.innerHTML = "";
+  filteredItems.forEach((item) => {
+    const article = document.createElement("article");
+    article.innerHTML = `
+          <img src="${item.image}" alt="${item.name}" />
+          <i class="fas fa-heart"></i>
+          <p>${item.price.toLocaleString()}원</p>
+          <h3>${item.name}</h3>
+        `;
+    itemWrapper.appendChild(article);
+  });
+};
+
+// 카테고리에 따라서 itemList를 필터링 하는 함수 (과일,일식,서적,전체)
+function filterItems(category) {
+  if (category === "전체") {
+    return SHOPPING_LIST;
+  } else {
+    const filteredItems = SHOPPING_LIST.filter(
+      (item) => item.category === category
+    );
+    return filteredItems;
+  }
+}
+
+// nav에서 a태그에 클릭 이벤트 추가
+document.querySelector(".nav-link-wrapper").addEventListener("click", (e) => {
+  if (e.target.tagName === "LI") {
+    const category = e.target.innerText;
+    itemTitle.innerText = category;
+    const filteredItems = filterItems(category);
+    displayItems(filteredItems);
+    e.preventDefault();
+  }
+});
+
+displayItems(SHOPPING_LIST);
