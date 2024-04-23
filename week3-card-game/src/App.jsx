edd629fa114a05c6ff52c5c59abcac030ba3bee4
suwonthugger <ddww1029wwdd@gmail.com>
2024-04-24 04:26:56 +0900

    feat: header 구현 및 theme color 변경 + 폰트 사이즈 추가

--- week3-card-game/src/App.jsx
+++ week3-card-game/src/App.jsx
@@ -1,5 +1,5 @@
-import GameBoard from "./components/GameBoard";
-import Header from "./components/Header";
+import GameBoard from "@/components/GameBoard";
+import Header from "@/components/Header";
 
 function App() {
   return (
--- week3-card-game/src/styles/GlobalStyles.js
+++ week3-card-game/src/styles/GlobalStyles.js
@@ -3,6 +3,32 @@ import reset from "styled-reset";
 
 const GlobalStyles = createGlobalStyle`
   ${reset}
+  a{
+        text-decoration: none;
+        color: inherit;
+    }
+    *{
+        box-sizing: border-box;
+    }
+    html, body, div, span, h1, h2, h3, h4, h5, h6, p, 
+    a, dl, dt, dd, ol, ul, li, form, label, table{
+        margin: 0;
+        padding: 0;
+        border: 0;
+        color: #FCF9F3;
+    }
+    body{
+        font-family: "Black Han Sans", sans-serif;
+        font-weight: 400;
+        font-style: normal;
+    }
+    ol, ul{
+        list-style: none;
+    }
+    button {
+        border: 0;
+        cursor: pointer;
+    }
 `;
 
 export default GlobalStyles;
