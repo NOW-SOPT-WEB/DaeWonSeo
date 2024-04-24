5fa0ede8e65c724dd53816fd73b56b590e78f286
suwonthugger <ddww1029wwdd@gmail.com>
2024-04-24 23:22:56 +0900

    feat: 난이도별로 랜덤하게 카드 생성 기능 구현

--- week3-card-game/src/components/GameBoard/_components/MonsterCard/index.jsx
+++ week3-card-game/src/components/GameBoard/_components/MonsterCard/index.jsx
@@ -1,16 +1,12 @@
 import styled from "styled-components";
 import backImageUrl from "@/assets/images/card-background.png";
-import frontImage from "@/assets/images/pig.png";
 
-export default function MonsterCard({
-  frontImageUrl = frontImage,
-  description,
-}) {
+export default function MonsterCard({ frontImageUrl, description }) {
   return (
     <FlipContainer>
       <CardContainer>
-        <FrontImage src={frontImageUrl} alt="description" />
-        <BackImage src={backImageUrl} />
+        <FrontImage src={frontImageUrl} alt={description} />
+        <BackImage src={backImageUrl} alt={"카드 뒷면 핑크빈 이미지"} />
       </CardContainer>
     </FlipContainer>
   );
