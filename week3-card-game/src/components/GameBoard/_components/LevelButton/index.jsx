690346812a6fa079ca86fb514e89ce953224c3c0
suwonthugger <ddww1029wwdd@gmail.com>
2024-04-24 13:57:22 +0900

    feat: 버튼 레이아웃 구현

--- week3-card-game/src/components/GameBoard/_components/LevelButton/index.jsx
+++ week3-card-game/src/components/GameBoard/_components/LevelButton/index.jsx
@@ -5,10 +5,14 @@ export default function LevelButton({ children }) {
 }
 
 const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   border: 0.2rem solid ${({ theme }) => theme.colors.secondary};
   border-radius: 0.5rem;
-  background: ${({ theme }) => theme.colors.primary};
+  background-color: ${({ theme }) => theme.colors.primary};
   width: 10rem;
   height: 4rem;
+  font-size: ${({ theme }) => theme.fontSize.md};
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.08);
 `;
--- week3-card-game/src/styles/GlobalStyles.js
+++ week3-card-game/src/styles/GlobalStyles.js
@@ -11,7 +11,7 @@ const GlobalStyles = createGlobalStyle`
         box-sizing: border-box;
     }
     html, body, div, span, h1, h2, h3, h4, h5, h6, p, 
-    a, dl, dt, dd, ol, ul, li, form, label, table{
+    a, dl, dt, dd, ol, ul, li, form, label, table, button{
         margin: 0;
         padding: 0;
         border: 0;
@@ -26,7 +26,9 @@ const GlobalStyles = createGlobalStyle`
         list-style: none;
     }
     button {
-        border: 0;
+        font-family: "Black Han Sans", sans-serif;
+        font-weight: 400;
+        font-style: normal;
         cursor: pointer;
     }
 `;
