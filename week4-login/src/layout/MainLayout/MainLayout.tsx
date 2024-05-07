2c763c12f2c4038c2bb07582e1b3184682435bfb
suwonthugger <ddww1029wwdd@gmail.com>
2024-05-07 16:58:17 +0900

    design: 레이아웃 디자인 구현

--- week4-login/src/layout/MainLayout/MainLayout.tsx
+++ week4-login/src/layout/MainLayout/MainLayout.tsx
@@ -2,8 +2,10 @@ import { Outlet } from "react-router-dom";
 
 function MainLayout() {
   return (
-    <main>
-      <Outlet />
+    <main className="flex justify-center items-center bg-main-gradient h-screen text-white">
+      <div className="bg-primary w-[36rem] h-[48rem] ">
+        <Outlet />
+      </div>
     </main>
   );
 }
--- week4-login/tailwind.config.js
+++ week4-login/tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1f1f1f",
+      },
+      backgroundImage: {
+        "main-gradient": `linear-gradient( #121212, #1a1a1a )`,
+      },
+    },
   },
   plugins: [],
 };
