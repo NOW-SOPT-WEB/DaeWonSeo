77806119ffe40fee0c0c6b4b9b46c425414d6d03
suwonthugger <ddww1029wwdd@gmail.com>
2024-05-07 22:35:29 +0900

    feat: 로그인 페이지 구현

--- week4-login/src/components/UserButton.tsx
+++ week4-login/src/components/UserButton.tsx
@@ -1,9 +1,14 @@
 interface UserButtonProps {
 	title: string;
+	onClick: () => void;
 }
 
-function UserButton({ title }: UserButtonProps) {
-	return <button className="bg-secondary rounded p-2 font-medium text-black">{title}</button>;
+function UserButton({ title, onClick }: UserButtonProps) {
+	return (
+		<button onClick={onClick} className="bg-secondary h-10 w-24 rounded p-2 font-medium text-black">
+			{title}
+		</button>
+	);
 }
 
 export default UserButton;
--- week4-login/src/pages/SignInPage/SignInPage.tsx
+++ week4-login/src/pages/SignInPage/SignInPage.tsx
@@ -1,5 +1,35 @@
+import { useNavigate } from 'react-router-dom';
+
+import UserInput from '@/components/UserInput';
+import UserButton from '@/components/UserButton';
+import { MAIN_ROUTES } from '@/constants/routes';
+import loginImage from '@/assets/images/login-title.png';
+
 function SignInPage() {
-	return <div>로그인</div>;
+	const navigate = useNavigate();
+
+	const handleClickSignUp = (): void => {
+		navigate(MAIN_ROUTES.SIGN_UP.path);
+	};
+
+	const handleClickSignIn = (): void => {
+		navigate(MAIN_ROUTES.SIGN_IN.path);
+	};
+
+	return (
+		<>
+			<h1 className="mb-20 text-5xl font-bold">Login</h1>
+			<img src={loginImage} className="h-48 rounded" />
+			<div className="mt-20 flex h-full w-full flex-col items-center gap-14">
+				<UserInput title={'ID'} />
+				<UserInput title={'비밀번호'} />
+			</div>
+			<div className="flex gap-9">
+				<UserButton title={'로그인'} onClick={handleClickSignIn} />
+				<UserButton title={'회원가입'} onClick={handleClickSignUp} />
+			</div>
+		</>
+	);
 }
 
 export default SignInPage;
