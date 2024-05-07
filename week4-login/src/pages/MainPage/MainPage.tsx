3b6b38091205b090678e009a60ba963df34171da
suwonthugger <ddww1029wwdd@gmail.com>
2024-05-08 02:36:14 +0900

    feat: 메인 페이지 구현

--- week4-login/src/pages/MainPage/MainPage.tsx
+++ week4-login/src/pages/MainPage/MainPage.tsx
@@ -1,5 +1,32 @@
+import video from '@/assets/videos/week4-video.mp4';
+import UserButton from '@/components/UserButton';
+import { MAIN_ROUTES } from '@/constants/routes';
+import { useNavigate } from 'react-router-dom';
+
 function MainPage() {
-  return <div>메인페이지</div>;
+	const navigate = useNavigate();
+
+	const handleClickMyPage = (): void => {
+		navigate(`${MAIN_ROUTES.MY_PAGE.path.replace(':id', '1')}`);
+	};
+
+	const handleClickSignUp = (): void => {
+		navigate(MAIN_ROUTES.SIGN_UP.path);
+	};
+
+	return (
+		<>
+			<div>
+				<video muted autoPlay loop>
+					<source src={video} />
+				</video>
+			</div>
+			<div className="flex gap-9">
+				<UserButton title={'내 정보'} onClick={handleClickMyPage} />
+				<UserButton title={'회원가입'} onClick={handleClickSignUp} />
+			</div>
+		</>
+	);
 }
 
 export default MainPage;
